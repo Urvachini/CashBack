2ea53c38149d5175a219ec370ac2e0c6397ad31a
Maria Alesenko <alesenkomaria@gmail.com>
2020-04-18 00:22:12 -0400

    dependenses added

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void shouldCalculateCashBack() {
+        CashbackHackService service = new CashbackHackService();
+            int amount = 2000;
+            int boundary = 100;
+
+            int actual = service.calculateRemain(amount);
+            int expected = 10;
+
+            assertEquals(actual, expected);
+        }
+    }
+}
