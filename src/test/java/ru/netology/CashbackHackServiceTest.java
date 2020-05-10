c32d3c217a530fb1a43a5c5fa56c35bf33a8eddc
Maria Alesenko <alesenkomaria@gmail.com>
2020-05-10 19:16:04 -0400

    Junit4 shouldCalculateCashBackReal renewed

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -14,7 +14,7 @@ public class CashbackHackServiceTest {
         int amount = 2000;
 
         int actual = service.remain(amount);
-        int expected = 1000;
+        int expected = 0;
 
         assertEquals(expected, actual);
     }
